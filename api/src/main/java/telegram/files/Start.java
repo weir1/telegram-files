e16f3e5882c599ac9baf83ba4591f9fceff29833
jarvis2f <137974272+jarvis2f@users.noreply.github.com>
2025-01-09 13:26:02 +0800

    🔖 0.1.8

--- api/src/main/java/telegram/files/Start.java
+++ api/src/main/java/telegram/files/Start.java
@@ -9,7 +9,7 @@ import java.util.concurrent.CountDownLatch;
 public class Start {
     private static final Log log = LogFactory.get();
 
-    public static final String VERSION = "0.1.7";
+    public static final String VERSION = "0.1.8";
 
     private static final CountDownLatch shutdownLatch = new CountDownLatch(1);
 
